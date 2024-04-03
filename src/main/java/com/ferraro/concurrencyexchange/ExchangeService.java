bd0a9dcd02240fec15eebfdafeeb4165f1fdc1cb
Domenico Ferraro <domy781@gmail.com>
2024-04-03 14:45:32 +0200

    made key as project variable and improved the ui

--- src/main/java/com/ferraro/concurrencyexchange/ExchangeService.java
+++ src/main/java/com/ferraro/concurrencyexchange/ExchangeService.java
@@ -3,6 +3,7 @@ package com.ferraro.concurrencyexchange;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
@@ -12,12 +13,14 @@ public class ExchangeService {
     private RestTemplate template = new RestTemplate();
     private ObjectMapper mapper = new ObjectMapper();
 
-    private String API_KEY = "fca_live_WadNSRXS4OGm1Pr6UNkdXW1Sn9EHoKmuzZodHCJi";
-    private String baseUrl = "https://api.freecurrencyapi.com/v1/latest?apikey=" + API_KEY;
+    @Value("${app.api.key}")
+    private String API_KEY;
+    private String baseUrl = "https://api.freecurrencyapi.com/v1/latest?apikey=";
 
 
     public Double getResult(Double givenValue, Currency baseCurrency, Currency convertTo){
-       String requestUrl = baseUrl+"&base_currency="+baseCurrency+"&currencies="+convertTo;
+        System.out.println(API_KEY);
+       String requestUrl = baseUrl+API_KEY+"&base_currency="+baseCurrency+"&currencies="+convertTo;
         JsonNode node;
         try {
             System.out.println(baseUrl);
